f7abbaf833d685056a33c2887a46a5bc5c0be136
James Lopez <tenjikrym@gmail.com>
2018-09-30 13:57:52 -0400

    Testing

--- index.php
+++ index.php
@@ -1,29 +1,19 @@
-<?php
-$servername = "127.0.0.1";
-$username = "admin";
-$password = "monarchs";
-//$username = "root";
-//$password = "";
-$dbname = "social_users";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-$sql = "SELECT * FROM oduScores;";
-$result = $conn->query($sql);
-echo "<html><body>\n";
-if ($result->num_rows > 0) {
-    echo "<table padding=2 border=1>\n";
-    echo "<tr><th>ID<th>opponent<th>visitor points<th>odu points<th>date\n";
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<td>" . $row["id"] . "<td>" . $row["opponent"] . "<td>" . $row["visitorPoints"] . "<td>" . $row["oduPoints"] . "<td>" . $row["notes"] . "\n";
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();
-echo "</body></html>";
-?>
+<!doctype>
+<html>
+	<head>
+		<title>Social Media Prototype Testing</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow" rel="stylesheet">
+	</head>
+	<body>
+		<div class="header">
+			<div id ="wrapper">
+				<div id="menu">
+					<a href="#" />Home</a>
+					<a href="#" />Sign Up</a>
+					<a href="#" />Sign In</a>
+				</div>
+			</div>
+		</div>	
+	</body>
+</html>
